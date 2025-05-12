61a8a21000f031d0b8e7042ba46f468faa9e0fd0
GonzaloB02 <131916311+GonzaloB02@users.noreply.github.com>
2025-05-12 10:55:41 -0300

    Add files via upload

---
+++ Lista_Pares/Main.py
@@ -0,0 +1,10 @@
+from Filling import filling_list, filtering_list
+
+def main():
+    list_1 = []
+
+    filling_list(list_1)
+    print(filtering_list(list_1))
+
+if __name__ == "__main__":
+    main()
